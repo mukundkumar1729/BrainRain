d5d969cc64c31c589decd2a05b4ecce453e6b294
mukundkumar1729 <51031256+mukundkumar1729@users.noreply.github.com>
2020-03-28 21:39:44 +0530

    3/28/2020
    3/28/2020

--- src/JSFunction.js
+++ src/JSFunction.js
@@ -1,122 +1,217 @@
 let quesAnsData = [];
-$(document).ready(function(){
-  GetDataOnPageLoad();
-         });
+$(document).ready(function() {
+    GetDataOnPageLoad();
+    $(window).resize(function() {
+        AlignSearchBox();
+    });
+});
 
-         function GetDataOnPageLoad(){
-          var fileUploadPath= './data/dataRepository.json';
-          if(quesAnsData.length == 0){
-            $.ajax({
-              url: fileUploadPath,
-              contentType:'json',
-              success: function(data,status){
-                  ActionOnPageLoad(data.quesAns);
-                  quesAnsData = data.quesAns; 
-                },
-                error: function(err, status){
-                }
-            });
-          }else{
-            ActionOnPageLoad(quesAnsData);
-          }
-         }
+function GetDataOnPageLoad() {
+    var fileUploadPath = './data/dataRepository.json';
+    if (quesAnsData.length == 0) {
+        $.ajax({
+            url: fileUploadPath,
+            contentType: 'json',
+            success: function(data, status) {
+                ActionOnPageLoad(data.quesAns);
+                quesAnsData = data.quesAns;
+            },
+            error: function(err, status) {}
+        });
+    } else {
+        ActionOnPageLoad(quesAnsData);
+    }
+}
 
-         function ActionOnPageLoad(data){
-          ShowSingleDiv();
-          ProcessQuesAns(data);
-          AlignSearchBox();
-         }
-function HideShow(e){
+function ActionOnPageLoad(data) {
+    ShowSingleDiv();
+    ProcessQuesAns(data);
+    AlignSearchBox();
+}
 
-  let domainTypeValue =  `data-domain-${e.value == "all" ? "" : e.value}`;
-  const table = $('#dvQuesAnsTable').DataTable(); 
-  table.search(domainTypeValue).draw();
+function HideShow(e) {
+
+    let domainTypeValue = `data-domain-${e.value == "all" ? "" : e.value}`;
+    const table = $('#dvQuesAnsTable').DataTable();
+    table.search(domainTypeValue).draw();
     $('input[type="search"]').val('');
 }
 
-    function ProcessQuesAns(data){
-      if(quesAnsData.length > 0){
+function ProcessQuesAns(data) {
+    if (quesAnsData.length > 0) {
         $('#dvQuesAnsTable').DataTable().clear().destroy();
-      }
-        var html = '';
-        let options = [];
-        for(var item of data){
-            options.includes(item.domain) ? '' : options.push(item.domain);
-            if(item.ques.trim() != '' ){
-                html += '<tr><td>';
-                html += `<span style='display:none'>data-domain-${item.domain.toLowerCase()}</span>`;
-                html += CreateElementForQuesAns(item.ques,item.domain,'ques');
-                html += CreateElementForQuesAns(item.ans,item.domain,'ans');
-                html += '</td></tr>';
-            }
+    }
+    var html = '';
+    let options = [];
+    for (var item of data) {
+        options.includes(item.domain) ? '' : options.push(item.domain);
+        if (item.ques.trim() != '') {
+            html += '<tr><td>';
+            html += `<span style='display:none'>data-domain-${item.domain.toLowerCase()}</span>`;
+            html += CreateElementForQuesAns(item, 'ques');
+            html += CreateElementForQuesAns(item, 'ans');
+            html += '</td></tr>';
         }
-        CreateOptionForQuesAns(options);
-        var dvQuesAns = document.getElementById('dvQuesAns');
-        dvQuesAns.innerHTML = '';
-        dvQuesAns.innerHTML= html;
-        
-        $('#dvQuesAnsTable').DataTable({
-          "order":[],
-          "lengthChange": true,
-          "pageLength": 25,
-          dom: 'Bfrtip',
-          buttons: [
-            'copy', 'csv', 'excel', 'pdf','print'
+    }
+    CreateOptionForQuesAns(options);
+    var dvQuesAns = document.getElementById('dvQuesAns');
+    dvQuesAns.innerHTML = '';
+    dvQuesAns.innerHTML = html;
+
+    $('#dvQuesAnsTable').DataTable({
+        "order": [],
+        "lengthChange": true,
+        "pageLength": 25,
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
         ]
-        });
-    }
+    });
+}
 
-    function CreateElementForQuesAns(item, domain, style){
-        style = style == 'ques' ? "background-color:white;margin-top:10px" :"background-color:green";
-        let row = `<div class='row'>
-                    <div class='col-md-12 col-12' style='${style}'>${item}</div></div>`;
-        return row;
-    }
+function CreateElementForQuesAns(item, qnsType) {
+    style = qnsType == constant.ques ? "background-color:white;margin-top:10px" : "background-color:green";
+    const editButton = `<button type='button' class='btn btn-primary' onclick='PreUpdateQuesAns("${item.ID}")' style='float:right;margin-right:-15px'>Edit</button>`;
+    let row = `<div class='row'>
+                        <div class='col-md-12 col-12' style='${style}'>
+                        ${qnsType == constant.ques ? item.ques : item.ans}
+                        ${qnsType == constant.ques ? editButton : ''}
+                        </div></div>`;
+    return row;
+}
 
-    function CreateOptionForQuesAns(options){
-      var ddl = document.getElementsByClassName('domain')[0];
-      ddl.options.length = 0;
-        ddl.options[ddl.options.length] = new Option("All", "all");
-      for(option of options) {
+function CreateOptionForQuesAns(options) {
+    var ddl = document.getElementsByClassName('domain')[0];
+    ddl.options.length = 0;
+    ddl.options[ddl.options.length] = new Option("All", "all");
+    for (option of options) {
         ddl.options[ddl.options.length] = new Option(option.toUpperCase(), option.toLowerCase());
     }
-  }
-	
-	function Redirect(page){
-		window.location.href = page+ '.html';
-  }
+}
 
-  function AlignSearchBox(){
-		const width = screen.width;
-		if(width <= 640){
-      $("#dvQuesAnsTable_filter").css({"margin-top": "0", "float": "left"});
-		}
-  }
-  
-  function CreateQuesAns(){
-    var quesObj = quesAnsData[quesAnsData.length-1];
-    const id=quesObj.ID + 1;
-    const quesID =quesObj.quesID + 1;
+function SaveQuesAns(el) {
+    debugger;
+    const mode = $(el).val();
+    if (mode == constant.update) {
+        UpdateQuesAns(el);
+    } else {
+        CreateQuesAns();
+    }
+}
+
+function PreCreateQuesAns() {
+    debugger;
+    ShowSingleDiv(control.updateBrainRain);
+    let el = $(constant.saveButton);
+    $(el).val(constant.add.toLowerCase());
+    $(el).text(constant.add);
+    $(`${control.updateBrainRain} .domain`)[0].disabled = false;
+}
+
+
+
+function CreateQuesAns() {
+    var quesObj = quesAnsData[quesAnsData.length - 1];
+    const id = quesObj.ID + 1;
+    const quesID = quesObj.quesID + 1;
     let ddl = $('#updateBrainRain .domain')[0];
     const domain = ddl.value == null ? "misc" : ddl.value;
     const question = $('#question').val();
-    const answer =$('#answer').val();
+    const answer = $('#answer').val();
     var quesAns = {
-      "ID":id,
-			"quesID":quesID,
-			"user":"kmukund439@gmail.com",
-			"domain":domain,
-			"ques":question,
-			"ans":answer
+        "ID": id,
+        "quesID": quesID,
+        "user": "kmukund439@gmail.com",
+        "domain": domain,
+        "ques": question,
+        "ans": answer
     };
     quesAnsData.push(quesAns);
-    if(confirm('Do you want to go to ques ans lists ?')){
-      GetDataOnPageLoad();
+    if (confirm('Do you want to go to ques ans lists ?')) {
+        GetDataOnPageLoad();
     }
-  }
+}
 
-  function ShowSingleDiv(div){
+function PreUpdateQuesAns(id) {
+    debugger;
+    ShowSingleDiv(control.updateBrainRain);
+    let el = $(control.saveButton);
+    $(el).val(constant.update.toLocaleLowerCase());
+    $(el).text(constant.update);
+    $(el).attr('itemID', id);
+    $(`${control.updateBrainRain} .domain`)[0].disabled = true;
+    $(control.question).val(quesAnsData[id - 1].ques);
+    $(control.answer).text(quesAnsData[id - 1].ans);
+}
+
+function UpdateQuesAns(el) {
+    debugger;
+    const id = $(el).attr("itemID");
+    const ques = $(control.question).val();
+    const ans = $(control.answer).val();
+    if (quesAnsData[id].ques == ques && quesAnsData[id].ans == ans) {
+        alert(constant.alertNoChange);
+        return false;
+    } else {
+        quesAnsData[id - 1].ques = ques;
+        quesAnsData[id - 1].ans = ans;
+    }
+
+    if (confirm(constant.confirmListing)) {
+        GetDataOnPageLoad();
+    }
+}
+
+function DeleteQuesAns(id) {
+    if (confirm(constant.confirmDelete)) {
+        quesAnsData[id - 1].isActive = 0;
+    } else {
+        return false;
+    }
+}
+
+
+function ShowSingleDiv(div) {
+    debugger;
     $('.spa-div').hide();
-    div = div == null ? 'listing' : div;
-    $('#'+ div).show();
-  }
+    div = div == null ? control.listing : div;
+    $(div).show();
+}
+
+function AlignSearchBox() {
+    const width = screen.width;
+    if (width <= 640) {
+        $("#dvQuesAnsTable_filter").css({
+            "margin-top": "0",
+            "float": "left"
+        });
+    }
+}
+
+const constant = {
+    update: "Update",
+    create: "Create",
+    add: "Add",
+    all: "All",
+    misc: "Misc",
+    ques: "ques",
+    ans: "ans",
+    empty: "",
+    dataDomain: "data-domain",
+    confirmDelete: "Are you sure to delete ?",
+    confirmListing: "Do you want to go to ques ans lists ?",
+    alertNoChange: "you didn't made any change to update"
+}
+
+const control = {
+    spaDiv: ".spa-div",
+    listing: "#listing",
+    dvQuesAnsTable: "#dvQuesAnsTable",
+    dvQuesAns: "#dvQuesAns",
+    dvQuesAnsTable_filter: "#dvQuesAnsTable_filter",
+    updateBrainRain: "#updateBrainRain",
+    saveButton: "#saveButton",
+    label: "label",
+    question: "#question",
+    answer: "#answer"
+}
