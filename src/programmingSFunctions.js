523bdd0e6302fdb3524866aa03bdc2c7acd580ca
mukundkumar1729 <51031256+mukundkumar1729@users.noreply.github.com>
2020-06-20 19:22:44 +0530

    changes
    changes

--- src/programmingSFunctions.js
+++ src/programmingSFunctions.js
@@ -3,19 +3,11 @@ $(document).ready(function(){
 });
 
 function GetPrograms_JSONFile() {
-    var filePath = './data/programmingRepository.json';
-    $.ajax({
-        url: filePath,
-        contentType: 'json',
-        success: function(data, status) {
-            GetPrograms(data.programs);
-        },
-        error: function(err, status) {
-        }
-    });
+    GetPrograms(programs);
 }
 
 function GetPrograms(data){
+    debugger;
     let el = $(control.CSharpProgramming);
     let html = "";
     for(let program of data){
@@ -28,6 +20,141 @@ function GetPrograms(data){
 }
 
 function ImplementAccordion(){
+    debugger;
     $('details').removeAttr('open')
-    $(this).parent().attr('open','open');
+    const el = $(this).parent();
+    const isOpen = $(el)[0].hasAttribute('open');
+    if(isOpen == 'open'){
+        $(this).parent().removeAttr('open');
+    }else{
+        $(this).parent().attr('open','open');
+    }
 }
+
+var programs =[
+    {
+        ID:1,
+        quesID:1,
+        user:"mukund439@gmail.com",
+        domain:"prg",
+        ques:"How to print pascal triangle in C# ?",
+        ans:`
+        using System;<br/>
+        using System.Collections.Generic;<br/>
+        using System.Linq;<br/>
+        using System.Text;<br/>
+        using System.Threading.Tasks; <br/><br/>
+        
+        namespace PascalTriangle <br/>
+        { <br/>
+            class Program <br/>
+            {<br/>
+                static void Main(string[] args) <br/>
+                { <br/>
+                    Console.WriteLine("Enter Order of Pascal triangle"); <br/>
+                    int order = int.Parse(Console.ReadLine()); <br/>
+                    int[] arr1 = new int[25]; <br/>
+                    int[] arr2 = new int[25]; <br/>
+                    arr1[0] = 1;          //holds the previous ordr element <br/>
+                    arr2[0] = 1;          //holds the current order eement <br/>
+                    for (int i = 0; i <= order; i++) <br/>
+                    { <br/>
+                        Console.Write("{0}:  ", i);<br/>
+                        for (int space = order; space > i; space--) <br/>
+                        { <br/>
+                            Console.Write("  "); <br/>
+                        } <br/>
+                        for (int j = 0; j <= i; j++) <br/>
+                        { <br/> <br/>
+                            if (j != 0) <br/>
+                                arr2[j] = arr1[j] + arr1[j - 1]; <br/>
+                            Console.Write(arr2[j] + "  "); <br/>
+                        } <br/>
+                        Array.Copy(arr2, arr1, arr2.GetLength(0)); <br/>
+                        Console.WriteLine("\n"); <br/>
+                    } <br/>
+                    Console.WriteLine("\n\n To find specific binomial coefficient enter 1"); <br/>
+                    int pass = int.Parse(Console.ReadLine()); <br/>
+                    if (pass == 1) <br/>
+                    { <br/>
+                        Console.WriteLine("Enter the value of n and r"); <br/>
+                        int n = int.Parse(Console.ReadLine()); <br/>
+                        int r = int.Parse(Console.ReadLine()); <br/>
+                        Console.WriteLine("Binomial Coefficient is : {0}", BinomialCoefficient(n, r)); <br/> 
+                        Console.ReadLine(); <br/>
+                    } <br/>
+                } <br/>
+                static double BinomialCoefficient(int n, int r) <br/>
+                { <br/>
+                    if (r == 0) <br/>
+                        return 1.0; <br/>
+                    return ((double)n / r) * BinomialCoefficient((n - 1), (r - 1)); <br/>
+                } <br/>
+            } <br/>
+        } <br/>
+        `,
+        CreatedDate:"2020/06/12/0"
+	},
+	{
+        ID:2,
+        quesID:2,
+        user:"mukund439@gmail.com",
+        domain:"prg",
+        ques:"How to swap two numbers without using third variable in C# ??",
+        ans:`
+             Answer ??? <br/> Answer ??? <br/> Answer ??? <br/>
+        `,
+        CreatedDate:"2020/06/12/0"
+	},
+	{
+        ID:1,
+        quesID:1,
+        user:"mukund439@gmail.com",
+        domain:"prg",
+        ques:"Finding count of a character in a word in C# ?",
+        ans:`
+            Answer ??? <br/> Answer ??? <br/> Answer ??? <br/>
+        `,
+        CreatedDate:"2020/06/12/0"
+	},
+	{
+        ID:1,
+        quesID:1,
+        user:"mukund439@gmail.com",
+        domain:"prg",
+        ques:"Question",
+        ans:`
+            Answer ??? <br/> Answer ??? <br/> Answer ??? <br/>
+        `,
+        CreatedDate:"2020/06/12/0"
+	},
+	{
+        ID:5,
+        quesID:5,
+        user:"mukund439@gmail.com",
+        domain:"prg",
+        ques:"Write a code to print X ?",
+        ans:`
+        <br/>
+        int r = 5;
+        int col = 2 * r - 1;
+        int row = 2 * r;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(j == i || j == col-i-1)
+                    {
+                        Console.Write((i < r) ? i : row - i -2);
+                    }
+                    else
+                    {
+                        Console.Write("		");
+                    }
+            }
+            Console.WriteLine();
+        } <br/>
+        `,
+        CreatedDate:"2020/06/12/0"
+	}
+];
