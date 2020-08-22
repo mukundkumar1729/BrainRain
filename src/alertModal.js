fd0612283a9d5c85ddc10f6e2006baa5dd2f7313
mukundkumar1729 <51031256+mukundkumar1729@users.noreply.github.com>
2020-08-22 11:47:52 +0530

    updates
    updates

--- src/alertModal.js
+++ src/alertModal.js
@@ -34,7 +34,7 @@ setTimeout(() => {
       </div>
       <div class="modal-body">
           <p>Download App for better browsing </p>
-          <button onclick="Transfer('${data.urls[constant.page]}_Aapp')" class="form-control btn btn-info">Download App</button>
+          <button onclick="Transfer('${data.urls[constant.page + '_Aapp']}')" class="form-control btn btn-info">Download App</button>
           ${appLinkHtml}
       </div>
       <div class="modal-footer">
