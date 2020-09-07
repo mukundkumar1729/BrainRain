f6a405f51e7593111bf22937ba38f412ab5f6dcd
mukundkumar1729 <51031256+mukundkumar1729@users.noreply.github.com>
2020-09-07 22:30:49 +0530

    updates
    updates

--- src/contactUs.js
+++ src/contactUs.js
@@ -16,7 +16,7 @@ $('#contactUsSection').append(appsLink);
 
 function ContactUsSocialMediaLinks(){
     const socialMediaLinks = `
-    <a onclick="RedirectTo('https://play.google.com/store/apps/developer?id=Mukund+Kumar/')" title="Visit Google Play"><img src="${baseUrl}image/googlePlay.jpg" alt="Google Play"/></a><span style="margin-right:5px;"></span>
+    <a onclick="RedirectTo('https://play.google.com/store/apps/developer?id=Mukund+Kumar')" title="Visit Google Play"><img src="${baseUrl}image/googlePlay.jpg" alt="Google Play"/></a><span style="margin-right:5px;"></span>
     <a onclick="RedirectTo('https://kmukund439@gmail.com/')" title="kmukund439@gmail.com"><img src="${baseUrl}image/gmail.png" alt="gmail"/></a></a><span style="margin-right:5px;"></span>
     <a onclick="RedirectTo('https://www.linkedin.com/in/mukundkumar1729/?originalSubdomain=in')"><img src="${baseUrl}image/LinkedIn.png" alt="linkedIn"/></a></a><span style="margin-right:5px;"></span>
     <a onclick="RedirectTo('https://twitter.com/mukundkumar1729')"><img src="${baseUrl}image/twitter.png" alt="twitter"/> </a></a><span style="margin-right:5px;"></span>
@@ -36,7 +36,7 @@ function ContactUsSocialMediaLinks(){
 
 function RedirectToCurrentApp(){
     if(!appUrl || appUrl.trim() == ""){
-        window.location.href= 'https://play.google.com/store/apps/developer?id=Mukund+Kumar/';
+        window.location.href= 'https://play.google.com/store/apps/developer?id=Mukund+Kumar';
     }else{
         window.location.href=appUrl;
     }
