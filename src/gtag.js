889d01df5ff483c832c4ef1dda6828c8bd5afa5b
mukundkumar1729 <51031256+mukundkumar1729@users.noreply.github.com>
2020-12-21 15:29:38 +0530

    google analytics

---
+++ src/gtag.js
@@ -0,0 +1,10 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-J79P30XQKX"></script>
+
+$.getScript("https://www.googletagmanager.com/gtag/js?id=G-J79P30XQKX");
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J79P30XQKX');
